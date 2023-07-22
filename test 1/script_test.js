d15f3d971920fe4c17336143fb6a0e46c55e204d
Ad31ide <seanchendu@gmail.com>
2023-07-21 23:40:43 -0500

    drop box

--- test 1/script_test.js
+++ test 1/script_test.js
@@ -14,3 +14,44 @@ var gameCanvas = {
                 document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         }
 }
+
+var player;
+var playerYPosition = 200
+
+function startGame(){
+        gameCanvas.start();
+
+        player = new createPlayer(30,30,10)
+}
+
+function createPlayer(width,height,x){
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = playerYPosition;
+
+        this.draw = function(){
+                ctx = gameCanvas.context;
+                ctx.fillStyle = 'green';
+                ctx.fillRect(this.x, this.y, this.width, this.height)     
+        }
+        
+        this.makeFall = function(){
+                this.y += fallSpeed;
+                fallSpeed += 0.1;
+                this. stopPlayer();
+        }
+
+}
+
+var fallSpeed = 0;
+var interval = setInterval(updateCanvas,20);
+
+function updateCanvas(){
+        ctx = gameCanvas.context;
+        ctx.clearRect(0,0,canvasWidth,canvasHeight);
+
+        player.makeFall()
+        player.draw()
+}
+
